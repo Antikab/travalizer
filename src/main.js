ebd0256819df06f1bcbed085f4243c6736844fd2
Arseny Prosvetov <arsprosvet@gmail.com>
2025-03-28 11:25:02 +0300

    start

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { initializeApp } from 'firebase/app'
+import App from './App.vue'
+import router from './router'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBh8-Xe5Lc-Y3Eu2fi61v8r9gPj_DLCVWU',
+  authDomain: 'travalizer-4a7d0.firebaseapp.com',
+  projectId: 'travalizer-4a7d0',
+  storageBucket: 'travalizer-4a7d0.firebasestorage.app',
+  messagingSenderId: '885971237913',
+  appId: '1:885971237913:web:9fcb4687eff84b1dd9fe88'
+}
+
+initializeApp(firebaseConfig)
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <main>Hello Vue3</main>
+</template>
